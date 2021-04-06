0a2f71607cd73ae6457220e558cb05c853f96bbc
Matheus Chiarelli <matheus.hchiarelli@gmail.com>
2021-04-06 18:05:02 -0300

    validation update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,6 +4,15 @@ import bcrypt from "bcrypt";
 class UserController {
   async index(req, res) {
     const { name, email, password } = req.body;
+
+    const userExist = await User.findOne({ where: { email } });
+
+    if (userExist) {
+      return res
+        .status(400)
+        .json({ error: "Usuário já existe, insira um email diferente" });
+    }
+
     if (!name) {
       return res.status(400).json({ message: "erro" });
     }
